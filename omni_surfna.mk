37056f59055982cbdc79b463c5cb8a3cc040bc7a
PizzaG <49663808+PizzaG@users.noreply.github.com>
2020-06-21 20:29:34 -0500

    v1.1
    TWRP Branch
    Fixed Booting Issue
    Broken: External SD Card

--- omni_surfna.mk
+++ omni_surfna.mk
@@ -18,10 +18,10 @@
 #
 
 # Specify phone tech before including full_phone
-#$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit some common Omni stuff.
-#$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit Telephony packages
